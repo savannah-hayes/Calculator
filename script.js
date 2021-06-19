5f921411893225f54022acf94328d2b0b3abaa8b
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-06-19 11:54:19 +0200

    Displaying with 1 key clicked

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".keys");
+const display = document.querySelector(".display");
 
+keys.addEventListener("click", (event) => {
+  if (event.target.matches("button")) {
+    const key = event.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    if (!action) {
+      if (displayedNum === "0") {
+        display.textContent = keyContent;
+      }
+    }
+  }
+});
