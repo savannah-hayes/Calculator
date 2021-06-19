05b2a9a94ac54aa30bf7e356da2a7ef8d99197e6
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-06-19 12:53:29 +0200

    Displaying decimal on click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".keys");
 const display = document.querySelector(".display");
+const previousKeyType = calculator.dataset.previousKeyType;
 
 keys.addEventListener("click", (event) => {
   if (event.target.matches("button")) {
@@ -8,10 +9,31 @@ keys.addEventListener("click", (event) => {
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
     if (!action) {
       if (displayedNum === "0") {
         display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
       }
     }
+
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
+    }
+
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
   }
 });
